566bd7a638de78436eb33de50ba648e2040fc6c3
Mian Saqib <saqibrza7@gmail.com>
2021-12-09 12:22:25 +0500

    first commit

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -8,7 +8,6 @@ public class ToasterMessage {
     public static void s(Context c, String message){
 
         Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
-        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
 
     }
 
